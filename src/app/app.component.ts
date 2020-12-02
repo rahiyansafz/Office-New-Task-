0478dd8165dee07e4ab148706444267b789d0acc
rahiyansafz <rahiyansafin@gmail.com>
2020-12-02 15:59:03 +0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -160,8 +160,3 @@ export class AppComponent {
     this.nameObj = "";
   }
 }
-// const months = ['Jan', 'March', 'April', 'June'];
-// months.splice(1, 0, 'Feb');
-// // inserts at index 1
-// console.log(months);
-// // expected output: Array ["Jan", "Feb", "March", "April", "June"]
